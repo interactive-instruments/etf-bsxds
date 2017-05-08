47692fda7f7e49655364d619051627df759630b1
Jon Herrmann <herrmann@interactive-instruments.de>
2017-05-08 09:57:50 +0200

    Spotless

--- src/main/java/de/interactive_instruments/etf/dal/dao/basex/BsxDataStorage.java
+++ src/main/java/de/interactive_instruments/etf/dal/dao/basex/BsxDataStorage.java
@@ -396,7 +396,7 @@ public final class BsxDataStorage implements BsxDsCtx, DataStorage {
 			throw new InitializationException("FunctX XQuery Function Library installation failed. "
 					+ "If a proxy server is used, set the Java Virtual Machine parameter 'http.proxyHost'. "
 					+ "Otherwise download functx-1.0.xar manually, extract the file (it is a ZIP file) and copy "
-					+ "it to the BaseXRepo folder 'repo' below folder "+storeDir.getAbsolutePath(), e);
+					+ "it to the BaseXRepo folder 'repo' below folder " + storeDir.getAbsolutePath(), e);
 		}
 
 		// Do not use auto optimize here: "However, updates can take much longer, so this option
